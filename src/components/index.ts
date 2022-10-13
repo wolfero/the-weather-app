94000b7ac877b0cf563689c37c8be3c99bd515e4
Wolfero <ferorock44@gmail.com>
2022-10-13 19:05:30 +0100

    🧑‍💻 improving better imports and exports of components folder

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import { ColorModeSwitcher } from './ColorModeSwitcher';
+import { TemperatureButtons } from './TemperatureButtons';
+import { SearchBar } from './SearchBar';
+import { Carousel } from './Carousel';
+import { Card } from './Card';
+
+export { ColorModeSwitcher, TemperatureButtons, SearchBar, Carousel, Card };

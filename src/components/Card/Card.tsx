45bd95e73b7894ee95cf2b19a7179459e05617b4
Wolfero <ferorock44@gmail.com>
2022-10-20 17:25:46 +0100

    💄 fix weather details style

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -17,15 +17,37 @@ const Card = ({ day }: { day: Day }) => {
 				<Box fontSize={'2rem'}>{temperature}º</Box>
 			</VStack>
 			<HStack mt={4} justifyContent={'center'}>
-				<Grid sx={cardGrid}>
-					<GridItem textAlign={'left'}>Feels like:</GridItem>
-					<GridItem textAlign={'right'}>{feelsLike}º</GridItem>
-					<GridItem textAlign={'left'}>Wind:</GridItem>
-					<GridItem textAlign={'right'}>{day.wind.speed} m/s</GridItem>
-					<GridItem textAlign={'left'}>Humidity:</GridItem>
-					<GridItem textAlign={'right'}>{day.main.humidity}%</GridItem>
-					<GridItem textAlign={'left'}>Pressure:</GridItem>
-					<GridItem textAlign={'right'}>{day.main.pressure} hPa</GridItem>
+				<Grid
+					sx={cardGrid}
+					templateAreas={`"description_1 value_1"
+                        "description_2 value_2"
+                        "description_3 value_3"
+                        "description_4 value_4"`}
+				>
+					<GridItem area={'description_1'} textAlign={'left'}>
+						Feels like:
+					</GridItem>
+					<GridItem area={'value_1'} textAlign={'right'}>
+						{feelsLike}º
+					</GridItem>
+					<GridItem area={'description_2'} textAlign={'left'}>
+						Wind:
+					</GridItem>
+					<GridItem area={'value_2'} textAlign={'right'}>
+						{day.wind.speed} m/s
+					</GridItem>
+					<GridItem area={'description_3'} textAlign={'left'}>
+						Humidity:
+					</GridItem>
+					<GridItem area={'value_3'} textAlign={'right'}>
+						{day.main.humidity}%
+					</GridItem>
+					<GridItem area={'description_4'} textAlign={'left'}>
+						Pressure:
+					</GridItem>
+					<GridItem area={'value_4'} textAlign={'right'}>
+						{day.main.pressure} hPa
+					</GridItem>
 				</Grid>
 			</HStack>
 		</Box>

b33ff2290eacdbf293ba21e0469fd1e268e94f38
Wolfero <ferorock44@gmail.com>
2022-10-20 12:19:43 +0100

    ✨ add weatherData selectors

---
+++ src/store/slices/weatherData/selectors.ts
@@ -0,0 +1,4 @@
+export const getCod = (state) => state.weatherData.cod;
+export const getMessage = (state): string => state.weatherData.message;
+export const getCityName = (state): string => state.weatherData.cityName;
+export const getTemperatureUnits = (state): string => state.weatherData.temperatureUnits;
